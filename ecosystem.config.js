dcf48d4b4ef301bbf8062a3914481cc17958a2cc
Jerome Vial <jerome.vial@gmail.com>
2025-02-22 12:10:33 +0100

    add dist to ignore watch

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,20 +3,30 @@ module.exports = {
     name: 'mtrl-app',
     script: 'server.ts',
     interpreter: 'bun',
-    watch: true,
-    ignore_watch: ['node_modules', 'src/client'],
+    watch: ['server.ts', 'src/server'],
+    ignore_watch: [
+      'node_modules',
+      'src/client',
+      'dist' // Don't watch dist directory
+    ],
     env: {
-      NODE_ENV: 'development'
+      NODE_ENV: 'development',
+      PORT: 4000
     },
     env_production: {
-      NODE_ENV: 'production'
+      NODE_ENV: 'production',
+      PORT: 4000
     }
   }, {
     name: 'mtrl-build',
     script: 'build.js',
     interpreter: 'bun',
     watch: ['src/client'],
-    ignore_watch: ['node_modules', 'src/server'],
+    ignore_watch: [
+      'node_modules',
+      'src/server',
+      'dist' // Don't watch dist directory
+    ],
     env: {
       NODE_ENV: 'development'
     },
