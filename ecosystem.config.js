c402aae59c21b351ab266509ac1a4aafb6c67098
Jerome Vial <jerome.vial@gmail.com>
2025-02-22 12:17:10 +0100

    update package.json and ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,37 +1,14 @@
+// ecosystem.config.js
 module.exports = {
   apps: [{
     name: 'mtrl-app',
     script: 'server.ts',
     interpreter: 'bun',
     watch: ['server.ts', 'src/server'],
-    ignore_watch: [
-      'node_modules',
-      'src/client',
-      'dist' // Don't watch dist directory
-    ],
+    ignore_watch: ['node_modules', 'dist'],
     env: {
       NODE_ENV: 'development',
       PORT: 4000
-    },
-    env_production: {
-      NODE_ENV: 'production',
-      PORT: 4000
-    }
-  }, {
-    name: 'mtrl-build',
-    script: 'build.js',
-    interpreter: 'bun',
-    watch: ['src/client'],
-    ignore_watch: [
-      'node_modules',
-      'src/server',
-      'dist' // Don't watch dist directory
-    ],
-    env: {
-      NODE_ENV: 'development'
-    },
-    env_production: {
-      NODE_ENV: 'production'
     }
   }]
 }
