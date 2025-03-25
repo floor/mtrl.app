452690f2eae411960e9c8aa02254711d2c75c750
Jerome Vial <jerome.vial@gmail.com>
2025-03-25 05:28:28 +0100

    add range slider discrete

---
+++ client/content/components/sliders/range-discrete.js
@@ -0,0 +1,30 @@
+import {
+  createComponentsSectionLayout
+} from '../../../layout'
+
+import {
+  createLayout,
+  createSlider
+} from 'mtrl'
+
+import {
+  SLIDER_COLORS
+} from 'mtrl/src/components/slider'
+
+export const initRangeDiscrete = (container) => {
+  const title = 'Range slider with tick marks'
+  const layout = createLayout(createComponentsSectionLayout({ title }), container).component
+
+  const slider = createSlider({
+    min: 0,
+    max: 1000,
+    value: 200,
+    secondValue: 800,
+    range: true,
+    step: 100,
+    ticks: true,
+    color: SLIDER_COLORS.SECONDARY
+  })
+  // btn.on('click', () => components.logEvent(`${variant} button clicked`))
+  layout.body.appendChild(slider.element)
+}
